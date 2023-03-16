17fd19cc12fab3861d5427c76600b0ed8d6c1ace
mevcaus <mevludincausevic@gmail.com>
2023-03-16 11:44:03 -0600

    added a .env plugin to webpack so that we can call process.env.auth client side

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-
+const webpack = require('webpack');
+require("dotenv").config();
 module.exports = {
   entry: path.join(__dirname, './client/src/index.jsx'),
   output: {
@@ -10,7 +11,13 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  plugins: [],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'AUTH_SECRET': process.env.AUTH_SECRET
+      }
+    })
+  ],
   module: {
     rules: [
       {
