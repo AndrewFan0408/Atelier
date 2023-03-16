15f38de9b015b0db828ab61b778a9c5ce652b03f
mevcaus <mevludincausevic@gmail.com>
2023-03-16 14:51:02 -0600

    only showing 2 answers unless button is clicked

--- client/src/components/Q&A/AnswerList.jsx
+++ client/src/components/Q&A/AnswerList.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import AnswerItem from './AnswerItem';
 
 const AnswerList = ( { answers } )=> {
-  console.log('these are my answers\n', answers)
+  const [numberOfAnswers, setNumberOfAnswers] = React.useState(2);
+  console.log(answers);
+  const handleShowMoreQuestionsClick = () => {
+    setNumberOfAnswers(Object.values(answers).length);
+  }
   return (
     <div>
-      {Object.values(answers).map(answer => {
+      {Object.values(answers).slice(0, numberOfAnswers).map(answer => {
         return <AnswerItem answer={answer} key={answer.id} />
       })}
+      {numberOfAnswers < Object.values(answers).length && (
+        <button onClick={handleShowMoreQuestionsClick}>Load More Answers</button>
+      )}
     </div>
   )
 }
