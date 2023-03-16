16559fe86122ce9c142907e1b52e2ebabe83ad00
mevcaus <mevludincausevic@gmail.com>
2023-03-16 15:50:34 -0600

    added helpful link that turns off after a vote

--- client/src/components/Q&A/AnswerItem.jsx
+++ client/src/components/Q&A/AnswerItem.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 const AnswerItem = ( { answer } ) => {
   const date = new Date(answer.date);
   const formattedDate = `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`;
+  const [voted, setVoted] = React.useState(false);
+  console.log(answer);
+  const handleHelpfulClick = () => {
+    setVoted(!voted);
+  }
   return (
     <div>
       <p>A: {answer.body} </p>
       <p>by {answer.answerer_name === 'seller' ?
       <strong>{answer.answerer_name}</strong> :
       answer.answerer_name}, {formattedDate} </p>
-      <p>👍: {answer.helpfulness} </p>
+      <p>Helpful?
+        {!voted && (
+          <a onClick={handleHelpfulClick}> Yes ({answer.helpfulness})</a>
+        )}
+        {voted && (
+          <span>   Thanks for your feedback</span>
+        )}
+      </p>
     </div>
   )
 }
