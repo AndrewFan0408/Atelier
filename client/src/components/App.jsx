f723652dc5583dba5666411acaf7cf87ff1f0d32
Vaughn Schneider <reelyvaughn@gmail.com>
2023-03-15 18:24:18 -0700

    added fonts, this is the last official commit

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,14 +7,17 @@ import RelatedItems from './RelatedItems';
 const App = () => {
   const [form, setForm] = React.useState(<div id="blank"><p>change your form here to view it, but <em>DO NOT</em> alter anything else in this app</p></div>);
 
-  //FILL IN YOUR COMPONENT BELOW
+  //FILL IN YOUR COMPONENT BELOW, COMMENT OUT TAGS AFTER VIEWING
   React.useEffect(() => {
     // setForm(<FILL_ME_IN />);
   }, [])
 
   return (
     <div>
-      <h1>Amyvify</h1>
+      <h1>Montserrat (h1)</h1>
+      <h3>Playfair Display (h3)</h3>
+      <h5>Roboto (h5)</h5>
+      <p>Lato (p)</p>
       <>{form}</>
     </div>
   )
