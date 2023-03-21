3dd2fbc4ee1be6474e91bc6869ae8cc64526b38c
mevcaus <mevludincausevic@gmail.com>
2023-03-21 00:07:01 -0600

    added add answer modal skeleton and refactored previous code to allow it

--- client/src/components/Q&A/Modal.jsx
+++ client/src/components/Q&A/Modal.jsx
@@ -4,15 +4,18 @@ const Modal = ( { product, handleClose, AnswerBoolean, question } ) => {
   const currentProduct = useSelector(state => state.product);
   const title = AnswerBoolean ? 'Submit your Answer' : 'Ask Your Question';
   const subTitle = AnswerBoolean ? `${product.name}: ${question.body}` : `About the ${product.name}`;
-  const [question, setQuestion] = React.useState('');
+  const [answerOrQuestion, setAnswerOrQuestion] = React.useState('');
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
-
+  const [error, setError] = React.useState('');
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    if (!answerOrQuestion || name || email) {
+
+    }
   }
-  const handleQuestionChange = (e) => {
-    setQuestion(e.target.value);
+  const handleAnswerOrQuestionChange = (e) => {
+    setAnswerOrQuestion(e.target.value);
   }
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -21,19 +24,15 @@ const Modal = ( { product, handleClose, AnswerBoolean, question } ) => {
     setEmail(e.target.value);
   }
   return (
-    <div className="modal">
+    <div className='modal'>
       <h1>{title}</h1>
       <h3>{subTitle}</h3>
       {AnswerBoolean ? (
         <form onSubmit={handleFormSubmit}>
-
-        </form>
-      ) : (
-        <form onSubmit={handleFormSubmit}>
-          <label>Your Question? (mandatory)</label>
+          <label>Your Answer? (mandatory)</label>
           <textarea
-          value={question}
-          onChange={handleQuestionChange}
+          value={answerOrQuestion}
+          onChange={handleAnswerOrQuestionChange}
           maxLength={1000}
           required
           />
@@ -55,7 +54,36 @@ const Modal = ( { product, handleClose, AnswerBoolean, question } ) => {
           required
           />
           <p>For authentication reasons, you will not be emailed.</p>
-          <button type="submit">Submit Question</button>
+          <button type='submit'>Submit Question</button>
+        </form>
+      ) : (
+        <form onSubmit={handleFormSubmit}>
+          <label>Your Question? (mandatory)</label>
+          <textarea
+          value={question}
+          onChange={handleAnswerOrQuestionChange}
+          maxLength={1000}
+          required
+          />
+          <label>What is your nickname? (mandatory)</label>
+          <textarea
+          value={name}
+          onChange={handleNameChange}
+          maxLength={60}
+          placeholder='Example: jackson11!'
+          required
+          />
+          <p>For privacy reasons, do not use your full name or email address.</p>
+          <label>What is your email? (mandatory)</label>
+          <textarea
+          value={email}
+          onChange={handleEmailChange}
+          maxLength={60}
+          placeholder='Why did you like the product or not?'
+          required
+          />
+          <p>For authentication reasons, you will not be emailed.</p>
+          <button type='submit'>Submit Question</button>
         </form>
       )}
     </div>
