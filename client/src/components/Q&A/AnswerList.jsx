c7028f0d05dd7f3a5325729a63d515f0acf3dc81
mevcaus <mevludincausevic@gmail.com>
2023-03-23 13:23:40 -0600

    fixed overflow css bug

--- client/src/components/Q&A/AnswerList.jsx
+++ client/src/components/Q&A/AnswerList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import AnswerItem from './AnswerItem';
 
-const AnswerList = ( { answers, question } )=> {
+function AnswerList({ answers, question }) {
   const [numberOfAnswers, setNumberOfAnswers] = React.useState(2);
-  const sortedAnswers = Object.values(answers).sort((a, b) => {
+  const sAnswers = Object.values(answers).sort((a, b) => {
     if (a.answerer_name === 'seller' && b.answerer_name !== 'seller') {
       return -1;
     }
@@ -14,25 +14,23 @@ const AnswerList = ( { answers, question } )=> {
   });
   const handleShowMoreAnswersClick = () => {
     setNumberOfAnswers(Object.values(answers).length);
-  }
+  };
   const handleShowLessAnswersClick = () => {
     setNumberOfAnswers(2);
-  }
+  };
 
   return (
     <div className="answer-list-container">
       <div className="answer-list">
-        {sortedAnswers.slice(0, numberOfAnswers).map(answer => {
-          return <AnswerItem answer={answer} key={answer.id} question={question}/>
-        })}
+        {sAnswers.slice(0, numberOfAnswers).map((a) => <AnswerItem answer={a} key={a.id} question={question} />)}
       </div>
-      {numberOfAnswers < sortedAnswers.length && (
-        <button onClick={handleShowMoreAnswersClick}>See More Answers</button>
+      {numberOfAnswers < sAnswers.length && (
+        <button onClick={handleShowMoreAnswersClick} type="button">See More Answers</button>
       )}
-      {numberOfAnswers === sortedAnswers.length && (
-        <button onClick={handleShowLessAnswersClick}>Collapse Answers</button>
+      {numberOfAnswers === sAnswers.length && (
+        <button onClick={handleShowLessAnswersClick} type="button">Collapse Answers</button>
       )}
     </div>
-  )
+  );
 }
-export default AnswerList
+export default AnswerList;
