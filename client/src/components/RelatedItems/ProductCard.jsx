4d3823caa45a56895c8d20b354bb26618c946f76
Yeongcheol Hwang <yeonghwang94@gmail.com>
2023-03-30 17:57:06 -0700

    files added for relatedItems

---
+++ client/src/components/RelatedItems/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProductCard = ({ product }) => {
+  console.log('inside productCard', product);
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default ProductCard;
